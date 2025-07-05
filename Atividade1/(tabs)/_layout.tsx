12af39a7c6cc1c9982d64a52252300e29454dd2f
Lia <146204711+juliajobbins@users.noreply.github.com>
2025-07-04 23:09:32 -0300

    Update _layout.tsx

--- Atividade1/(tabs)/_layout.tsx
+++ Atividade1/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
@@ -5,35 +6,31 @@ import React from "react";
 export default function Layout() {
   return (
     <Tabs
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName: keyof typeof Ionicons.glyphMap = "home";
+      screenOptions={({ route }) => {
+        let iconName: keyof typeof Ionicons.glyphMap;
 
-          if (route.name === "index") {
-            iconName = "home";
-          } else if (route.name === "profissional") {
-            iconName = "briefcase-outline";
-          }
+        if (route.name === "index") {
+          iconName = "home";
+        } else if (route.name === "profissional") {
+          iconName = "briefcase";
+        }
 
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: "#87cefa",
-        tabBarInactiveTintColor: "#888",
-        tabBarStyle: {
-          backgroundColor: "#0d0d1a",
-          borderTopColor: "#000080",
-          paddingBottom: 6,
-          paddingTop: 6,
-          height: 60,
-        },
-        headerStyle: {
-          backgroundColor: "#0d0d1a",
-        },
-        headerTintColor: "#87cefa",
-        tabBarLabelStyle: {
-          fontSize: 12,
-        },
-      })}
+        return {
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={iconName} size={size} color={color} />
+          ),
+          tabBarActiveTintColor: "#87cefa",
+          tabBarInactiveTintColor: "#888",
+          tabBarStyle: {
+            backgroundColor: "#0d0d1a",
+            borderTopColor: "#000080",
+            paddingBottom: 6,
+            paddingTop: 6,
+            height: 60,
+          },
+          headerShown: false,
+        };
+      }}
     />
   );
 }
